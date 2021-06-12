934a95117f963a14ac5ee5f08e051046f1c87a21
Yusuffrih <yusuffrih@hotmail.com>
2021-06-12 14:47:54 +0000

    Added key instructions in beginGame for UX

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,15 @@ let keyPattern = [];
 let playersPattern = [];
 
 const startButton = document.querySelector('.start-button');
+const instructions = document.querySelector('.key-instructions');
+
 
 function beginGame(){
     startButton.classList.add('hidden');
-
+    instructions.classList.remove('hidden');
+    instructions.innerHTML = `
+    <p>White Keys = Z,X,C,V,B,N,M</p>
+    <p>Black Keys = S,D,G,H,J</p>`
 }
 
 startButton.addEventListener('click', beginGame)
