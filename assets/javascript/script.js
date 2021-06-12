6efcb24927375ca6f3fc7c74a644b36fdaf437a7
Yusuffrih <yusuffrih@hotmail.com>
2021-06-12 18:39:31 +0000

    Changed heading const to tally

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,13 +4,19 @@ let level = 0;
 
 const startButton = document.querySelector('.start-button');
 const instructions = document.querySelector('.key-instructions');
-const heading = document.querySelector('.heading');
+const tally = document.querySelector('.tally');
 const pianoContainer = document.querySelector('.piano-container');
 
 
 function nextStage(){
     level += 1;
     
+    pianoContainer.classList.add('unclickable');
+    instructions.innerHTML = `
+    <p>White Keys = Z,X,C,V,B,N,M</p>
+    <p>Black Keys = S,D,G,H,J</p>`;
+    
+
     const nextKeyPattern = [...keyPattern];
     nextKeyPattern.push(nextKey());
     playStage(nextKeyPattern);
