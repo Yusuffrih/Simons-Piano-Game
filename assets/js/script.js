2907afeaef61f7839e9dd4807045acd4b6befd27
Yusuffrih <yusuffrih@hotmail.com>
2021-06-23 17:27:11 +0000

    Changed tally class to id attr.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const startButtonRef = document.querySelector('.start-button');
 const keyInstructionsRef = document.querySelector('#key-instructions');
-const tallyRef = document.querySelector('.tally');
+const tallyRef = document.querySelector('#tally');
 const pianoContainerRef = document.querySelector('#piano-container');
 const keysWhiteRef = document.querySelectorAll('.white-key');
 const keysBlackRef = document.querySelectorAll('.black-key');
